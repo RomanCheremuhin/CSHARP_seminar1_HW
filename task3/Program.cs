9a4053b8fdabdcce5e183bd928d8533ea2ee39de
Roman Cheremuhin <cheremuhinrv@gmail.com>
2024-04-11 01:40:22 +0300

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+
+void Main()
+{
+    int[] myArray = { 1, 2, 5, 0, 10, 34 };
+
+    Console.WriteLine("Элементы массива, начиная с конца:");
+    PrintArrayReverse(myArray, myArray.Length - 1);
+}
+
+void PrintArrayReverse(int[] arr, int i)
+{
+    if (i >= 0)
+    {
+        Console.Write(arr[i] + " ");
+        PrintArrayReverse(arr, i - 1);
+    }
+}
+Main();
